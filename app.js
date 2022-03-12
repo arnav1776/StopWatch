7263691910b5f03959780f54aee9ef8364257dc5
Arnav Sharma <arnav1776@gmail.com>
2022-03-12 19:47:38 +0530

    lap

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
 var button = document.querySelector(".start");
-// var miliseconds = document.querySelector(".milisecond");
 var seconds = document.querySelector(".second");
 var minutes = document.querySelector(".minute");
 var hour = document.querySelector(".hour");
 var lap = document.querySelector(".lap");
 var lapContainer = document.querySelector(".lap-container");
 
-var count = 1;
-// var milisec = 0;
+var count = 1
 var sec = 0;
+var hr = 0;
 var min = 0;
-var timerOn = false;
-var timer = false;
+var isTimerOn = false;
+var timerVar = false;
 button.addEventListener("click", function (event) {
-    if(timerOn)
-    {
-        clearInterval(timer);
-        button.innerHTML = "Start";
-        timerOn = false;
-        lap.innerHTML = "reset"
-    }
-    else {
-        timer = setInterval( function() {
-            // milisec = parseInt(milisec);
+    if(isTimerOn)
+  {
+    clearInterval(timerVar);
+    button.innerHTML = "Start";
+    isTimerOn = false;
+    lap.innerHTML = "Reset"
+  }
+  else
+  {
+        timerVar = setInterval(function () {
             sec = parseInt(sec);
             min = parseInt(min);
             hr = parseInt(hr);
-
-            sec = sec + 1;
-            // if (milisec == 60) {
-            //     sec = sec + 1;
-            //     milisec = 0;
-            // }
+            sec = sec+1;
             if (sec == 60) {
                 min = min + 1;
                 sec = 0;
+                milisec = 0;
             }
             if (min == 60) {
                 hr = hr + 1;
                 min = 0;
+                sec = 0;
+                milisec = 0;
             }
-            // if (milisec < 10 || milisec == 0) {
-            //     milisec = '0' + milisec;
-            // }
+           
             if (sec < 10 || sec == 0) {
                 sec = '0' + sec;
             }
@@ -52,15 +47,41 @@ button.addEventListener("click", function (event) {
             if (hr < 10 || hr == 0) {
                 hr = '0' + hr;
             }
-
-            // miliseconds.innerHTML = milisec;
-            seconds.innerHTML = sec;
-            minutes.innerHTML = min;
             hour.innerHTML = hr;
+            minutes.innerHTML = min;
+            seconds.innerHTML = sec;
         },1000);
-        timerOn = true;
-        button.innerHTML = "stop";
-        lap.innerHTML = "lap";
-    }
+        isTimerOn = true;
+        button.innerHTML = "stop"
+        lap.innerHTML = "lap"
+  }
+    
 });
 
+lap.addEventListener("click", function () {
+    var timeDiv = document.createElement("div");
+    timeDiv.setAttribute("id","timeStamp")
+    var hr = document.createElement("hr");
+    var timeStamp = document.createElement("span");
+    var lapCounter = document.createElement("span")
+    if(lap.innerHTML == 'lap'){
+    lapCounter.innerHTML = count;
+    count+=1;
+    timeStamp.innerHTML = `${hour.innerHTML}:${minutes.innerHTML}:${seconds.innerHTML}`
+    timeDiv.appendChild(lapCounter)
+    timeDiv.appendChild(timeStamp)
+    lapContainer.appendChild(timeDiv);
+    lapContainer.appendChild(hr);
+    }
+    else{
+        lapContainer.innerHTML = "";
+        hour.innerHTML = '00'
+        minutes.innerHTML = '00'
+        seconds.innerHTML = '00'
+        count = 1
+        sec = 0;
+        hr = 0;
+        min = 0;
+    }
+    
+})
